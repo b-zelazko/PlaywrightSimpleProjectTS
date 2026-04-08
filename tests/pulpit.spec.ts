2e917eab27716ecd017a3d4ebe8267c5e09fdd4c
b-zelazko <bartlomiej.zelazko@gmail.com>
2026-04-08 14:02:33 +0200

    test: verify correct balance after mobile top-up; add helper scripts to package.json

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -2,15 +2,15 @@ import { test, expect } from "@playwright/test";
 
 test.describe("Pulpit tests", () => {
   test.describe.configure({ retries: 3 });
-  
+
   test.beforeEach(async ({ page }) => {
     const userId = "testerLO";
     const userPassword = "12345678";
-    await page.goto('/');
+    await page.goto("/");
     await page.getByTestId("login-input").fill(userId);
     await page.getByTestId("password-input").fill(userPassword);
     await page.getByTestId("login-button").click();
-  
+
     await page.waitForLoadState("domcontentloaded");
     // wait for page to fully load:
   });
@@ -51,5 +51,23 @@ test.describe("Pulpit tests", () => {
     await expect(page.getByTestId("message-text")).toHaveText(
       `Doładowanie wykonane! ${topUpAmount},00PLN na numer ${topUpReceiver}`,
     );
+  });
+
+  test("Correct balance after successful mobile top-up", async ({ page }) => {
+    //Arrange
+    const topUpReceiver = "500 xxx xxx";
+    const topUpAmount = "100";
+    const initialBalance = await page.locator("#money_value").innerText();
+    const expectedBalance = Number(initialBalance) - Number(topUpAmount);
+
+    //Act
+    await page.locator("#widget_1_topup_receiver").selectOption(topUpReceiver);
+    await page.locator("#widget_1_topup_amount").fill(topUpAmount);
+    await page.locator("#uniform-widget_1_topup_agreement > span").click();
+    await page.getByRole("button", { name: "doładuj telefon" }).click();
+    await page.getByTestId("close-button").click();
+
+    //Assert
+    await expect(page.locator("#money_value")).toHaveText(`${expectedBalance}`);
   });
 });
